e761f343fb7f07818d806726f9926ca67413ed75
Daniele Cattaneo <daniele.cattaneo@alice.it>
2015-05-31 17:57:30 +0200

    Fix retain cycle.

--- M68kSimApp/M68kSimApp/MOSSource.h
+++ M68kSimApp/M68kSimApp/MOSSource.h
@@ -22,12 +22,12 @@
   /* AppKit already has a private window outlet, but it's not exposed
    * because NSDocument also supports multiple windows for the same document.
    * But, since we don't support that, it's just an annoyance. */
-  IBOutlet NSWindow *docWindow;
+  __weak IBOutlet NSWindow *docWindow;
   BOOL simulatorMode;
   
   NSTextStorage *text;
   SMLTextView *textView;
-  __strong IBOutlet MGSFragariaView *fragaria;
+  IBOutlet MGSFragariaView *fragaria;
   MOSFragariaPreferencesObserver *prefobs;
   
   IBOutlet MOSSimulatorViewController *simVc;
