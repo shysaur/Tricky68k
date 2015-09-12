68bbb19cb36cdd61d3fce63cc826ab3d543fc75e
Daniele Cattaneo <daniele.cattaneo@alice.it>
2015-09-12 21:51:26 +0200

    Read the TTY on a separate thread to avoid slowing down execution because of poll().
    The poll() code path is still there to support having the TTY and the debug console both reading from stdio.

--- m68ksim/m68ksim/tty.c
+++ m68ksim/m68ksim/tty.c
@@ -13,6 +13,7 @@
 #include <fcntl.h>
 #include <string.h>
 #include <poll.h>
+#include <pthread.h>
 #include "m68ksim.h"
 #include "tty.h"
 #include "addrspace.h"
@@ -20,9 +21,48 @@
 
 int tty_alreadyInstalled = 0;
 int tty_fildesout, tty_fildesin;
+volatile int16_t tty_char = -1;
+pthread_cond_t tty_bufCond;
+pthread_mutex_t tty_bufMutex;
+pthread_t tty_thread;
 
 
-uint16_t tty_read_16bit(struct segment_desc_s *me, uint32_t addr) {
+void *tty_bufferThread(void *param) {
+  int16_t c;
+  
+  pthread_detach(pthread_self());
+  
+  for (;;) {
+    c = 0;
+    if (read(tty_fildesin, &c, 1) == 1) {
+      pthread_mutex_lock(&tty_bufMutex);
+      tty_char = c;
+      pthread_cond_wait(&tty_bufCond, &tty_bufMutex);
+      tty_char = -1;
+      pthread_mutex_unlock(&tty_bufMutex);
+    }
+  }
+  
+  return NULL;
+}
+
+
+uint16_t tty_readWithBuffer_16bit(struct segment_desc_s *me, uint32_t addr) {
+  uint16_t read;
+  
+  if (tty_char == -1)
+    return -1;
+  
+  pthread_mutex_lock(&tty_bufMutex);
+  read = tty_char;
+  tty_char = -1;
+  pthread_cond_signal(&tty_bufCond);
+  pthread_mutex_unlock(&tty_bufMutex);
+  return read;
+}
+
+
+uint16_t tty_readWithPolling_16bit(struct segment_desc_s *me, uint32_t addr) {
   int16_t c;
   int avail;
   struct pollfd polls;
@@ -42,12 +82,12 @@ uint16_t tty_read_16bit(struct segment_desc_s *me, uint32_t addr) {
 
 
 uint32_t tty_read_32bit(struct segment_desc_s *me, uint32_t addr) {
-  return tty_read_16bit(me, addr) * 0x10000 + tty_read_16bit(me, addr+2);
+  return me->read_16bit(me, addr) * 0x10000 + me->read_16bit(me, addr+2);
 }
 
 
 uint8_t tty_read_8bit(struct segment_desc_s *me, uint32_t addr) {
-  return (tty_read_16bit(me, addr & ~1) >> (addr & 1 ? 0 : 8)) & 0xFF;
+  return (me->read_16bit(me, addr & ~1) >> (addr & 1 ? 0 : 8)) & 0xFF;
 }
 
 
@@ -74,9 +114,6 @@ error_t *tty_install(uint32_t base, int fildes_in, int fildes_out) {
   if (tty_alreadyInstalled)
     return error_new(401, "Can't install more than one TTY");
   
-  tty_fildesout = fildes_out;
-  tty_fildesin = fildes_in;
-  
   desc = calloc(1, sizeof(segment_desc));
   if (!desc)
     return error_new(402, "Failed to allocate TTY segment descriptor");
@@ -85,7 +122,6 @@ error_t *tty_install(uint32_t base, int fildes_in, int fildes_out) {
   desc->size = SEGM_GRANULARITY;
   desc->action = ACTION_ONREAD | ACTION_ONWRITE;
   desc->ident = (void*)tty_install;
-  desc->read_16bit = tty_read_16bit;
   desc->read_32bit = tty_read_32bit;
   desc->read_8bit = tty_read_8bit;
   desc->write_16bit = tty_write_16bit;
@@ -95,6 +131,25 @@ error_t *tty_install(uint32_t base, int fildes_in, int fildes_out) {
   if ((tmpe = mem_installSegment(desc))) {
     free(desc);
     return tmpe;
+  }
+  
+  if (fildes_out == STDOUT_FILENO)
+    tty_fildesout = dup(fildes_out);
+  else
+    tty_fildesout = fildes_out;
+  
+  if (fildes_in == STDIN_FILENO) {
+    tty_fildesin = dup(fildes_in);
+    desc->read_16bit = tty_readWithPolling_16bit;
+  } else {
+    tty_fildesin = fildes_in;
+    desc->read_16bit = tty_readWithBuffer_16bit;
+    if (pthread_cond_init(&tty_bufCond, NULL))
+      return error_new(402, "Failed to initialize TTY");
+    if (pthread_mutex_init(&tty_bufMutex, NULL))
+      return error_new(402, "Failed to initialize TTY");
+    if (pthread_create(&tty_thread, NULL, tty_bufferThread, NULL))
+      return error_new(402, "Failed to initialize TTY");
   }
   
   tty_alreadyInstalled = 1;
@@ -130,8 +185,8 @@ error_t *tty_installCommand(int special, int argc, char *argv[]) {
     if (outfd < 0)
       return error_new(413, "Can't open output file %s.", outf);
   } else {
-    outfd = dup(STDOUT_FILENO);
-    infd = dup(STDIN_FILENO);
+    outfd = STDOUT_FILENO;
+    infd = STDIN_FILENO;
   }
   
   return tty_install(base, infd, outfd);
