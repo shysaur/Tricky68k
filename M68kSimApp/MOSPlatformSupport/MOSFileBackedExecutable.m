d0685c1d4bb6221bcd59ad69b18e21e1c5439429
Daniele Cattaneo <daniele.cattaneo@alice.it>
2017-08-07 14:35:25 +0200

    Improve the errors returned by MOSFileBackedExecutable.

--- M68kSimApp/MOSPlatformSupport/MOSFileBackedExecutable.m
+++ M68kSimApp/MOSPlatformSupport/MOSFileBackedExecutable.m
@@ -38,14 +38,27 @@
 
 - (BOOL)writeToURL:(NSURL *)outf withError:(NSError **)errptr
 {
-  NSError *err;
-  NSFileManager *fm = [NSFileManager defaultManager];
-  BOOL res = [fm copyItemAtURL:self.executableFile toURL:outf error:&err];
-  if (res)
-    return YES;
-  if (errptr)
-    *errptr = err;
-  return NO;
+  /* Q: Why are you using NSData instead of NSFileManager or copyfile(3)? This
+   *    way you are missing out on all the APFS awesomeness!
+   * A: Because NEITHER of them actually returns ANY usable ERROR CODE.
+   *    IN 2017. * angrily punches a fist through the air * NSFileManager
+   *    always returns a non-descript error with an underlying NSError of
+   *    EIO, which is useless (and a lie unless your disk is broken,
+   *    so it's even a dangerous lie to tell). copyfile(3) is meant to return
+   *    an error in errno, but for some reason we actually always get zero...
+   *    which is a problem of errno more than copyfile(3) but come on, everyone
+   *    knows about that and designs their APIs so that they return an error
+   *    code instead of relying on errno in 2017... But, the authors of
+   *    copyfile are not capable of doing that. I must assume AppKit and the
+   *    POSIX layer are both maintained by the worst interns imaginable.
+   *    Now my faith in humanity has dropped to a new low, thanks Apple. */
+  NSData *tmp = [NSData dataWithContentsOfURL:self.executableFile
+    options:NSDataReadingMappedAlways error:errptr];
+  if (!tmp)
+    return NO;
+  if (![tmp writeToURL:outf options:0 error:errptr])
+    return NO;
+  return YES;
 }
 
 
